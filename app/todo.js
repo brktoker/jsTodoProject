56e57b127d0ece2c3c291370160f79b49debb703
tokerburak <brktoker@gmail.com>
2020-04-19 23:54:58 +0300

    add todo to local storage get set and alert function

--- app/todo.js
+++ app/todo.js
@@ -15,17 +15,73 @@ eventListener()
 function eventListener() {
 
     form.addEventListener("submit", addTodo)
+    document.addEventListener("DOMContentLoaded",loadAllTodosToUI)
+}
+
+function loadAllTodosToUI(){
+    let todos = getTodosFromStorage()
+    
+    todos.forEach(function(todo){
+        addUITodo(todo)
+    })
+
 }
 
 function addTodo(e) {
 
     const newTodo = todoInput.value.trim();// to prevent gaps trim() function
 
-    addUITodo(newTodo)
+    if (newTodo === "") {
 
+        showAlert("danger", "Lütfen Todo Giriniz")
+    }
 
+    else {
+
+        addUITodo(newTodo)
+        addTodoToStorage(newTodo)
+        showAlert("success", "Todo Ekleme Başarılı")
+    }
 
     e.preventDefault();//page is not refreshed 
+}
+
+function getTodosFromStorage(){
+    let todos
+
+    if (localStorage.getItem("todos") === null) {
+        todos = []
+    }
+    else {
+        todos = JSON.parse(localStorage.getItem("todos"))
+    }
+    return todos
+}
+
+
+function addTodoToStorage(newTodo){
+
+    let todos = getTodosFromStorage()
+
+    todos.push(newTodo)
+
+    localStorage.setItem("todos",JSON.stringify(todos))
+
+}
+
+
+// alert todo
+function showAlert(type, message) {
+
+    const alert = document.createElement("div")
+
+    alert.className = `alert alert-${type}`
+    alert.textContent = message
+    firstCardBody.appendChild(alert)
+
+    window.setTimeout(function () {
+        alert.remove()
+    }, 2000)
 }
 
 // add UI todo
@@ -44,8 +100,9 @@ function addUITodo(newTodo) {
     listItem.appendChild(document.createTextNode(newTodo))
     todoList.appendChild(listItem)
     todoInput.value = ""
-
-
 }
+
+
+
 
 
